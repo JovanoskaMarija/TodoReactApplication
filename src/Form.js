4b71ccc931d35352e996c4d360efc98c6e24d53e
Marija <marija.jovanoska.maca@gmail.com>
2019-09-27 16:48:17 +0200

    UPD style and disable button

--- src/Form.js
+++ src/Form.js
@@ -1,48 +1,26 @@
 import React, { Component } from "react";
 import shortid from "short-id";
-import styled from "styled-components";
 
-const AddTodoForm = styled.form`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: center;
-  padding: 3%;
-`;
 
-const AddTodoInput = styled.input`
-  width: 70%;
-  margin: 0.5rem;
-  border: none;
-  border-bottom: 2px solid #009999;
-`;
-
-const AddTodoButton = styled.button`
-  width: 20%;
-  padding: 0.5rem;
-  margin: 0.5rem;
-  border: 2px solid #009999;
-  border-radius: 5px;
-  background: #009999;
-  color: white;
-`;
+import {AddTodoForm, AddTodoInput, AddTodoButton} from './FormStyle.js'
 
 class Form extends Component {
-  constructor(props) {
-    super(props);
-  }
+  
   state = {
     text: ""
   };
 
   handleSubmit = e => {
     e.preventDefault();
-    this.props.addTodo({
-      text: this.state.text,
-      complete: false,
-      id: shortid.generate()
-    });
-
+    if(this.state.text.trim()){
+      this.props.addTodo({
+        text: this.state.text,
+        complete: false,
+        id: shortid.generate()
+      });
+      this.setState({ text: "" });
+    }
+    
     this.setState({ text: "" });
   };
 
@@ -53,6 +31,7 @@ class Form extends Component {
   };
 
   render() {
+
     return (
       <AddTodoForm onSubmit={this.handleSubmit}>
         <AddTodoInput
@@ -61,8 +40,8 @@ class Form extends Component {
             name="text"
             placeholder="Add todo.." >
         </AddTodoInput>
-        <AddTodoButton type="submit" disabled={!this.state.text}> Add Todo </AddTodoButton>
-          {/* <RenderCount /> */}
+        <AddTodoButton type="submit"  disabled={!this.state.text}> Add Todo </AddTodoButton>
+        {/* <RenderCount />  */}
       </AddTodoForm>
     );
   }
