66c7c819de9a37a30db0c1512031cd39aea65519
Marija <marija.jovanoska.maca@gmail.com>
2019-09-27 16:49:27 +0200

    UPD style

--- src/Todo.js
+++ src/Todo.js
@@ -1,42 +1,15 @@
-import React from "react";
-import styled from "styled-components";
+import React,{Component} from "react";
+import {TodoWrapper, Text, DeleteButton} from './TodoStyle.js'
 
-const TodoWrapper = styled.div`
-  display: flex;
-  flex-direction:row;
-  justify-content: space-between;
-  align-items: center;
-  width: 100%;
-  border-bottom: 2px solid #009999; 
-  margin: 2%;
-`;
-
-const TodoItem = styled.div`
-  margin: 2%;
-  
-`;
-
-const DeleteButton = styled.button`
-  width: 10%;
-  padding: 0.5rem;
-  margin: 0.5rem;
-  border: none;
-  background: transparent;
-  color:  #009999;
-  cursor: pointer;
-`;
 
 const Todo = props => {
+
   return (
     <TodoWrapper>
-      <TodoItem
-        style={{
-          textDecoration: props.todo.complete ? "line-through" : "none"
-        }}
-        onClick={props.toggleComplete}
-      >
+      <input type="checkbox" onClick={props.toggleComplete}/>
+      <Text checked={props.todo.complete} >
         {props.todo.text}
-      </TodoItem>
+      </Text>
       <DeleteButton onClick={props.deleteTodo} disabled={props.text}> X </DeleteButton>
     </TodoWrapper>
   );
